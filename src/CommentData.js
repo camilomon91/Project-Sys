54f207735fff6e597019778f49fc5b123ab4972c
Juan Camilo Montero Campo <monthero@Juans-MacBook-Air.local>
2024-08-30 16:55:36 -0400

    hola

--- src/CommentData.js
+++ src/CommentData.js
@@ -17,6 +17,6 @@ export const comments = [
     {
       profileImg: 'https://images.unsplash.com/photo-1563482776068-4dac10f9373d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
       username: 'BuryHeadInSand',
-      comment: 'Wild ostriches make the best pets.'
+      comment: 'Wild ostriches make the best petsss.'
     }
   ]
